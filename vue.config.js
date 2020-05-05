152b496648bbfe25f59ba2638b4c9406969b4719
Mariusz Augustyniak <maugustyniak@gmail.com>
2020-05-05 17:41:33 +0200

    localhost https certs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
   devServer: {
-    https: false,
+    https: {
+      key: fs.readFileSync('./certs/dev_localhost.key'),
+      cert: fs.readFileSync('./certs/dev_localhost.pem'),
+    },
   },
 };
