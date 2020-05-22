d5cacab71000f08ad83efcfacddab40a05055861
Mariusz Augustyniak <maugustyniak@gmail.com>
2020-05-22 20:43:24 +0200

    #622 Tasks component and task dialog wip

--- src/components/task/TaskViewDialog.vue
+++ src/components/task/TaskViewDialog.vue
@@ -17,44 +17,25 @@
         </v-card-title>
         <v-card-text>
           <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
+
+            <v-text-field label="Title" required></v-text-field>
+
+            <v-text-field label="Note" required></v-text-field>
+
+            <v-select
+              :items="[1, 2, 3, 4, 5]"
+              label="Priority"
+              required
+            ></v-select>
+
+            <v-autocomplete
+              :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball']"
+              label="Participants"
+              multiple
+            ></v-autocomplete>
+
           </v-container>
-          <small>*indicates required field</small>
+
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -70,6 +51,7 @@
 export default {
   data: () => ({
     dialog: false,
+
   }),
 };
 </script>
