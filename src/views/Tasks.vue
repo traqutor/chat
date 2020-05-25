cf8c016fc5a0c4823b1c5d38929a144e8595010d
Mariusz Augustyniak <maugustyniak@gmail.com>
2020-05-25 12:12:42 +0200

    Tasks view component

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,12 +1,48 @@
 <template>
-    <tasks-kanban-board />
+  <section>
+
+    <v-tabs
+    fixed-tabs
+    v-model="tab"
+    background-color="#18222d"
+    dark
+  >
+      <v-tab v-for="item in items" :key="item.tab">
+        {{ item.tab }}
+      </v-tab>
+  </v-tabs>
+
+
+  <v-tabs-items v-model="tab">
+    <v-tab-item
+      v-for="item in items"
+      :key="item.tab"
+    >
+      <component v-bind:is="item.content"></component>
+    </v-tab-item>
+  </v-tabs-items>
+
+  </section>
 </template>
 
 <script>
 import TasksKanbanBoard from '@/components/task/TasksKanbanBoard.vue';
+import SettingsSelector from '@/components/SettingsSelector.vue';
 
 export default {
   name: 'Tasks',
+  data() {
+    return {
+      tab: null,
+      items: [
+        { tab: 'Task List', content: 'TasksKanbanBoard' },
+        { tab: 'Map', content: 'SettingsSelector' },
+        { tab: 'Raise Task', content: 'SettingsSelector' },
+        { tab: 'Manage', content: 'SettingsSelector' },
+        { tab: 'Progress', content: 'SettingsSelector' },
+      ],
+    };
+  },
   components: {
     TasksKanbanBoard,
   },
